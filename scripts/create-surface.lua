37b45ce935a9a951dc7b3a5c89e1ea173593a288
Tom Burrows <tomburrows13@icloud.com>
2022-11-03 15:15:12 +0000

    Fix surface creation

--- scripts/create-surface.lua
+++ scripts/create-surface.lua
@@ -1,13 +1,13 @@
 function create_transformer_surface(surface_name)
   local new_surface_name = surface_name .. "-transformer"
   if not game.get_surface(new_surface_name) and string.sub(surface_name, -12) ~= "-transformer" then
-    local surface = game.create_surface(name)
+    local surface = game.create_surface(new_surface_name)
     surface.generate_with_lab_tiles = true
     surface.show_clouds = false
     surface.always_day = true
   
     if remote.interfaces["RSO"] and remote.interfaces["RSO"]["ignoreSurface"] then
-      remote.call("RSO", "ignoreSurface", name)
+      remote.call("RSO", "ignoreSurface", new_surface_name)
     end
   
     log("Creating transformer surface " .. new_surface_name)
