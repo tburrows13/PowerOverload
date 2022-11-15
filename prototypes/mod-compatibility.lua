2c16522518e857d77364bdcd633c0f0de4a17ec6
Tom Burrows <tomburrows13@icloud.com>
2022-11-15 01:28:31 +0000

    IR2: move transformer unlock to first electricity tech

--- prototypes/mod-compatibility.lua
+++ prototypes/mod-compatibility.lua
@@ -1,4 +1,16 @@
 -- data-updates
+if mods["IndustrialRevolution"] then
+  table.insert(data.raw.technology["ir2-steam-power"].effects, {
+    type = "unlock-recipe",
+    recipe = "po-transformer"
+  })
+
+  for i, effect in pairs(data.raw.technology["electric-energy-distribution-1"].effects) do
+    if effect.type == "unlock-recipe" and effect.recipe == "po-transformer" then
+      data.raw.technology["electric-energy-distribution-1"].effects[i] = nil
+    end
+  end
+end
 
 if mods["bzlead"] then
   table.insert(data.raw.recipe["po-huge-electric-pole"].ingredients, {"lead-plate", 10})
