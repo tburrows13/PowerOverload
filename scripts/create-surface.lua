9d507d795c497f8b39d2a9476bbf37a6d687f6cb
Tom Burrows <tomburrows13@icloud.com>
2024-12-10 00:22:27 +0000

    Hide transformer surfaces

--- scripts/create-surface.lua
+++ scripts/create-surface.lua
@@ -5,11 +5,15 @@ function create_transformer_surface(surface_name)
     surface.generate_with_lab_tiles = true
     surface.show_clouds = false
     surface.always_day = true
-  
+
+    for _, force in pairs(game.forces) do
+      force.set_surface_hidden(new_surface_name, true)
+    end
+
     if remote.interfaces["RSO"] and remote.interfaces["RSO"]["ignoreSurface"] then
       remote.call("RSO", "ignoreSurface", new_surface_name)
     end
-  
+
     log("Creating transformer surface " .. new_surface_name)
   end
 end
@@ -17,5 +21,14 @@ end
 function create_transformer_surfaces()
   for _, surface in pairs(game.surfaces) do
     create_transformer_surface(surface.name)
+  end
+  -- Runs every on_configuration_changed, which handles migration and sort-of new forces
+  for _, surface in pairs(game.surfaces) do
+    local surface_name = surface.name
+    if string.sub(surface_name, -12) == "-transformer" then
+      for _, force in pairs(game.forces) do
+        force.set_surface_hidden(surface_name, true)
+      end
+    end
   end
 end
