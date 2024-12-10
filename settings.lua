1538fd38dbbe61773d3def01843c6a41e9323c4d
Tom Burrows <tomburrows13@icloud.com>
2024-12-10 00:35:43 +0000

    Temporarily use unlocalised string in mod settings to workaround engine bug https://forums.factorio.com/124129

--- settings.lua
+++ settings.lua
@@ -41,7 +41,7 @@ for pole_name, default_power in pairs(shared.get_pole_names(mods)) do
       {
         type = "string-setting",
         name = "power-overload-max-power-" .. pole_name,
-        localised_name = {"", {"description.max-energy-consumption"}, ": [entity=" .. pole_name .. "] ", {"mod-setting-name.power-overload-entity", pole_name}},
+        localised_name = {"", {"description.max-energy-consumption"}, ": [entity=" .. pole_name .. "] ", pole_name},
 
         setting_type = "startup",
         default_value = default_power,
